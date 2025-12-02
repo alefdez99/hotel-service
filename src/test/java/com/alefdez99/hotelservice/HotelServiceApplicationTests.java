786d5332d611829c0cbe6881dda14abe79a10899
alefdez24 <alefdezbar@gmail.com>
2025-12-02 19:33:50 +0100

    First Commit: Project structure and basic pom.xml

---
+++ src/test/java/com/alefdez99/hotelservice/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alefdez99.hotelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
